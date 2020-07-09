39da7b5d8cd4a94dee7d7839282b5bede4badcef
core-melt <core.melt21@gmail.com>
2020-07-09 18:06:07 +0900

    [Create]rooms channle

---
+++ app/channels/room_channel.rb
@@ -0,0 +1,19 @@
+# サーバーサイドの処理を受け持つチャンネル
+# サーバーでWebSocketを受け付けるたびに、Connectionオブジェクトがインスタンス化する。
+# Connection自体は、アプリのロジックは扱わない。
+# ConnectionはApplicationCable::Connectionのインスタンスである。
+# Connectionオブジェクトは、全てのChannel Subscriptionsの親となる(2-1)。
+# Consumer: WebSocket Connectionのクライアント。
+# 各ユーザーが開くブラウザタブ、ウィンドウ、デバイスごとに、ConsumerのConnectionのペアが1つづつ作成される。
+class RoomChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def speak
+  end
+end
